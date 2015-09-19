bfa6e074d29d798e63e8ff293ab32511cfe93755
Trevor Edwards <tre23@cornell.edu>
2015-09-19 12:45:47 -0400

    merged gabe's work

--- src/app.js
+++ src/app.js
@@ -62,7 +62,10 @@ var titleToGame = function(e){
   game.show();
   titleMenu.hide();
   game.on('click','up', function(){
-    game.openAnimate( roomRender(0,true) );
+    game.openAnimate( );
+    game.on('click','up', function(){
+      game.transition( roomRender(0,true) );
+    });
   });
   //Initialize game
   //Render initial view
@@ -124,24 +127,221 @@ function roomRender(view, hasDoor){
      .animate('position',  new Vector2(72, 76), 500);
      
      setTimeout(function() {
-       knob.animate('position',  new Vector2(79, 75), 500);
+       knob.animate('position',  new Vector2(81, 76), 1000);
       }, 600);
      
      setTimeout(function() {
         knob.remove();
+      }, 1700);
+
+    
+ };
+    
+    wind.transition = function(nextRoom){
+      
+        knob.remove();
         mypic.remove();
         door.animate('position', new Vector2(0,0),1000)
             .animate('size', new Vector2(144,168),1000);
-      }, 1200);
      
       setTimeout(function() {
         nextRoom.show();
              wind.hide();
-      }, 2400);
-    
- };
+      }, 1050);
+      
+    };
   }
   
   return wind;
   
 }
+
+
+
+
+/**
+* BEGIN GAME.JS -------------------------------------------------------------------------
+*/
+
+function room(trap, displayText, xCoordinate, yCoordinate, zCoordinate) {
+        this.trap = trap;
+        this.displayText = displayText;
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+        this.zCoordiante = zCoordinate;
+}
+
+function Person (name) {
+        this.name = name;
+        this.NSEWDirection = "NORTH";
+        this.UDDirection = "STRAIGHT";
+
+        this.Items = [];
+}
+
+function Game(name, room, size, startx, starty, startz) {
+        var gam;
+        this.myPerson = Person(name);
+        this.mySize = size;
+        this.rooms = room[size][size][size];
+        this.generateRooms(size);
+        this.currentX = 0;
+        this.currentY = 0;
+        this.currentZ = 0;
+        return gam;
+}
+
+Game.prototype.generateRooms = function(size, displayNames) {
+        var possibleNames = [];
+        getDisplaynames(possibleNames);
+        for(var x = 0; x < size; x++){
+        for(var y = 0; y < size; y++){
+        for(var z = 0; z < size; z++){
+                this.rooms[x][y][z] = room("dank", possibleNames[x*y*z % possibleNames.length()], x, y, z);
+        }
+        }
+        }
+};
+
+function getDisplaynames(possibleNames) {
+        possibleNames.push("This is the dankest meme Room");
+        possibleNames.push("Congratulations, you win the room!");
+        possibleNames.push("Wow, this room has the rarest pepe room");
+        possibleNames.push("Trevor Edwards");
+        possibleNames.push("Darude Sandstorm");
+        possibleNames.push("duududududu");
+}
+
+//Used when you have an invalid funciton and you need to prompt a message to the player that 
+//are doing some screwed up shit
+var  displayInvalid = function() {
+        //do some shit
+};
+
+
+Game.prototype.attemptMove = function() {
+        if(this.currentRoom.currentX === 0 && this.myPerson.NSEWDirection == "WEST") {
+                displayInvalid();
+        } else if(this.currentRoom.currentX == this.mySize-1 && this.myPerson.NSEWDirection == "EAST") {
+                displayInvalid();
+        } else if(this.currentRoom.currentY === 0 &&this.myPersonNSEWDirection == "SOUTH"){
+                displayInvalid();
+        } else if(this.currentRoom.currentY == this.mySize-1 && this.myPerson.NSEWDirection == "NORTH") {
+                displayInvalid();
+        } else if(this.currentRoom.currentZ === 0 && this.myPerson.UDDirection == "DOWN") {
+                displayInvalid();
+        } else if(this.currentRoom.currentZ == this.mySize-1 && this.myPerson.UDDirection == "UP") {
+                displayInvalid();
+        } else {
+                this.interact();
+        }
+
+};
+
+Game.prototype.displayRoomInfo = function() {
+        //display room info based on this.rooms[this.currentX][this.currentY][this.currentZ].displayText;
+};
+
+Game.prototype.enter = function() {
+        if(this.myPerson.UDDirection != "STRAIGHT"){
+                switch(this.myPerson.UDDirection){
+                        case "UP":
+                                this.currentZ++;
+                                break;
+                                //execute based on trap here
+
+                        case "DOWN":
+                                this.currentZ--;
+                                break;
+                                //execute based on trap here
+                        default:        
+                                break;
+                                //what the FUCK
+                } } else {
+                switch(this.myPerson.NSEWDirection){
+                        case "NORTH":
+                                this.currentY++;
+                                break;
+                                //execute based on trap here
+                        case "SOUTH":
+                                this.currentY--;
+                                break;
+                                //execute based on trap here
+                        case "EAST":
+                                this.currentX++;
+                                break;
+                                //execute based on trap here
+                        case "WEST":
+                                this.currentX--;
+                                break;
+                                //execute based on trap here
+                        default: 
+                                break;
+                                //WTF
+                }
+        }
+        this.displayRoomInfo();
+        switch(this.rooms[this.currentX][this.currentY][this.currentZ].trap){
+                case "FINALDOOR":
+                case "FIERYDEATH":
+                case "EBOLA":
+                case "SPIKEDEATH":
+            case "GASDEATH":
+        }
+
+};
+
+Game.prototype.interactWithRoom = function() {
+        if(this.rooms[this.currentX][this.currentY][this.currentZ].trap == "do somethign"){
+                //display based on trap
+        }
+}
+          
+            
+            
+
+Person.prototype.turnRight = function () {
+        if(this.NSEWDirection == "NORTH") {
+                this.NSEWDirection = "EAST";
+        } else if(this.NSEWDirection == "EAST") {
+                this.NSEWDirection = "SOUTH";
+        } else if(this.NSEWDirection == "SOUTH") {
+                this.NSEWDirection = "WEST";
+        } else if(this.NSEWDirection  == "WEST") {
+                this.NSEWDirection = "NORTH";
+        }
+};
+
+Person.prototype.turnLeft = function () {
+        if(this.NSEWDirection == "NORTH") {
+                this.NSEWDirection = "WEST";
+        } else if(this.NSEWDirection == "WEST") {
+                this.NSEWDirection = "SOUTH";
+        } else if(this.NSEWDirection == "SOUTH") {
+                this.NSEWDirection = "EAST";
+        } else if(this.NSEWDirection  == "EAST") {
+                this.NSEWDirection = "NORTH";
+        }
+};
+
+Person.prototype.lookUP = function() {
+        if(this.UDDirection == "STRAIGHT") {
+                this.UDDirection = "UP";
+        } else if (this.UDDirection == "DOWN") {
+                this.UDDirection = "STRAIGHT";
+        }
+};
+
+Person.prototype.lookDOWN = function() {
+        if(this.UDDirection == "UP") {
+                this.UDDirection = "STRAIGHT";
+        } else if (this.UDDirection == "STRAIGHT") {
+                this.UDDirection = "DOWN";
+        }
+};
+
+Person.prototype.takeItem = function(item) {
+  
+  //TODO
+        //Items.push(item);
+} ;         
